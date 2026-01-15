155fae4dfa360d6d104489e5cb36ce904091f753
Christopher <krisstopher0715@gmail.com>
2026-01-15 11:36:39 -0400

    define mask off half byte macros

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,13 @@
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
 #define DISPLAY_SIZE DISPLAY_WIDTH * DISPLAY_HEIGHT
+#define STACK_SIZE 16
+#define MASKOFF_HHB(x) ((x & 0xF0) >> 4)
+#define MASKOFF_LHB(x) ((x & 0x0F))
 
 uint8_t memory[MEMORY_SIZE];
 uint8_t display[DISPLAY_SIZE];
+uint16_t stack[STACK_SIZE];
 
 uint8_t fonts[] = {
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
