cf21aa5e0e503cd1efe7493112fb785811971a12
Christopher <krisstopher0715@gmail.com>
2026-01-16 23:06:04 -0400

    cpu instruction functions

---
+++ src/include/inst.h
@@ -0,0 +1,11 @@
+#ifndef INST_H
+#define INST_H
+#include "emu.h"
+
+void clear_screen(struct emu *emu);
+void jump(struct emu *emu);
+void set_reg_VX(struct emu *emu);
+void add_reg_VX(struct emu *emu);
+void set_idx(struct emu *emu);
+
+#endif
---
+++ src/inst.c
@@ -0,0 +1,32 @@
+#include "include/emu.h"
+#include "include/mask.h"
+
+#include <string.h>
+
+static inline void inc_pc(uint8_t *pc_reg) {
+    *pc_reg += 2;
+}
+
+void clear_screen(struct emu *emu) {
+    memset(emu->display, 0, DISPLAY_SIZE);
+    inc_pc(&emu->pc_reg);
+}
+
+void jump(struct emu *emu) {
+    emu->pc_reg = MASKOFF_NNN(emu->pc_reg);
+}
+
+void set_reg_VX(struct emu *emu) {
+    emu->gp_regs[MASKOFF_X(emu->pc_reg)] = MASKOFF_NN(emu->pc_reg);
+    inc_pc(&emu->pc_reg);
+}
+
+void add_reg_VX(struct emu *emu) {
+    emu->gp_regs[MASKOFF_X(emu->pc_reg)] += MASKOFF_NN(emu->pc_reg);
+    inc_pc(&emu->pc_reg);
+}
+
+void set_idx(struct emu *emu) {
+    emu->idx_reg = MASKOFF_NNN(emu->pc_reg);
+    inc_pc(&emu->pc_reg);
+}
