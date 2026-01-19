b02b38aebd65a2cbec625681ef676e5336aa9680
Christopher <krisstopher0715@gmail.com>
2026-01-19 03:43:42 -0400

    fix macros

--- src/include/mask.h
+++ src/include/mask.h
@@ -1,12 +1,11 @@
 #ifndef MASK_H
 #define MASK_H
 
-#define MASKOFF_HB(b) ((b >> 12) & 0xF)
-#define MASKOFF_X(b) ((b >> 8) & 0xF)
-#define MASKOFF_Y(b) ((b >> 4) & 0xF)
-#define MASKOFF_N(b) (b & 0xF)
-#define MASKOFF_NN(b) (b & (MASKOFF_Y(b) | MASKOFF_N(b)))
-#define MASKOFF_NNN(b) ((b & (MASKOFF_X(b) | MASKOFF_Y(b) | MASKOFF_N(b))))
-// todo: add a opcode get macro
+#define MASKOFF_HB(b) (((b) >> 12) & 0xF)
+#define MASKOFF_X(b)  (((b) >> 8) & 0xF)
+#define MASKOFF_Y(b)  (((b) >> 4) & 0xF)
+#define MASKOFF_N(b)  ((b) & 0xF)
+#define MASKOFF_NN(b) ((b) & 0xFF)
+#define MASKOFF_NNN(b) ((b) & 0x0FFF)
 
 #endif
